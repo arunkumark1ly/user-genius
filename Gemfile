fadf2dacd4050551e395a5659364335715d6e063
Arun <k.arunkumar@outlook.in>
2024-06-08 00:51:20 +0530

    fetchuser job has been developed to test API call, and integrated byebug for debugging purposes.

--- Gemfile
+++ Gemfile
@@ -63,6 +63,9 @@ group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem 'web-console'
 
+  # Byebug is used for debugging Ruby code. It allows stepping through code and inspecting variables at runtime.
+  gem 'byebug'
+
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem 'rack-mini-profiler'
 
--- app/sidekiq/fetch_users_worker_job.rb
+++ app/sidekiq/fetch_users_worker_job.rb
@@ -2,6 +2,18 @@ class FetchUsersWorkerJob
   include Sidekiq::Job
 
   def perform(*args)
-    # Do something
+    # Fetch data from the API
+    response = HTTParty.get('https://randomuser.me/api/?results=20')
+    if response.success?
+      users_data = response.parsed_response['results']
+
+      # Iterate over each user data and store in the database
+      users_data.each do |user_data|
+        byebug
+      end
+    else
+      # Log error or handle it as needed
+      Rails.logger.error "Failed to fetch users: #{response.body}"
+    end
   end
 end
