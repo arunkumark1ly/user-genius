03148ebbf594aad43eb5c99610689cc32359bb71
Arun <k.arunkumar@outlook.in>
2024-06-08 22:10:29 +0530

    've developed a RecalculateAverageAgeJob to ensure the corresponding DailyRecord is updated when a user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
   validates :uuid, presence: true, uniqueness: true
 
+  before_destroy :enqueue_recalculation
+
   def full_name_with_title
     "#{name["title"]} #{name["first"]} #{name["last"]}"
-  end  
+  end
+
+  private
+
+  def enqueue_recalculation
+    daily_record_date = self.created_at.to_date
+    # Convert the date to a string in ISO 8601 format
+    RecalculateAverageAgeJob.perform_async(daily_record_date.iso8601, self.gender)
+  end
 end
---
+++ app/sidekiq/recalculate_average_age_job.rb
@@ -0,0 +1,22 @@
+class RecalculateAverageAgeJob
+  include Sidekiq::Worker
+
+  def perform(daily_record_date_str, gender)
+    daily_record_date = Date.iso8601(daily_record_date_str)
+    daily_record = DailyRecord.find_by(date: daily_record_date)
+    return unless daily_record
+
+    # Calculate the new average age
+    new_average = User.where(gender: gender, created_at: ..daily_record_date.end_of_day).average(:age).to_f
+
+    # Calculate the new count
+    new_count = User.where(gender: gender, created_at: daily_record_date.all_day).count
+
+    # Update the DailyRecord with new average age and count
+    if gender == 'male'
+      daily_record.update(male_avg_age: new_average, male_count: new_count)
+    else
+      daily_record.update(female_avg_age: new_average, female_count: new_count)
+    end
+  end
+end
