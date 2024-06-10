f09ccd4517ad8ff143c8373c20bed72b66faa441
Arun <k.arunkumar@outlook.in>
2024-06-10 20:14:12 +0530

    test case fix

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -4,12 +4,15 @@ require 'rails_helper'
 
 RSpec.describe 'users/index.html.erb', type: :view do
   before do
-    assign(:users, [
-             User.create!(uuid: 123, name: { 'last' => 'Kandasamy', 'first' => 'ArunKumar', 'title' => 'Mr' }, age: 30,
-                          gender: 'Male', created_at: Time.zone.now),
-             User.create!(uuid: 124, name: { 'last' => 'ArunKumar', 'first' => 'Megala', 'title' => 'Mrs' }, age: 25,
-                          gender: 'Female', created_at: Time.zone.now)
-           ])
+    users = double('paginated_users')
+    allow(users).to receive(:total_pages).and_return(1)
+    allow(users).to receive(:current_page).and_return(1)
+    allow(users).to receive(:limit_value).and_return(2)  # assuming per page limit is 2 for simplicity
+
+    allow(users).to receive(:each).and_yield(User.new(id: 1, name: { 'last' => 'Kandasamy', 'first' => 'ArunKumar', 'title' => 'Mr' }, age: 30, gender: "Male", created_at: '2023-01-02'))
+                                    .and_yield(User.new(id: 2, name: { 'last' => 'ArunKumar', 'first' => 'Megala', 'title' => 'Mrs' }, age: 25, gender: "Female", created_at: '2023-01-03'))
+
+    assign(:users, users)
     assign(:total_users, 2)
     render
   end
@@ -29,14 +32,16 @@ RSpec.describe 'users/index.html.erb', type: :view do
     expect(rendered).to include('<th>Name</th>')
     expect(rendered).to include('<th>Age</th>')
     expect(rendered).to include('<th>Gender</th>')
-    expect(rendered).to include('<th>Created At</th>')
-    expect(rendered).to include('<th>Actions</th>')
+
     expect(rendered).to include('<td>Mr ArunKumar Kandasamy</td>')
     expect(rendered).to include('<td>30</td>')
     expect(rendered).to include('<td>Male</td>')
+    expect(rendered).to include('<td>2023-01-02</td>')
+
     expect(rendered).to include('<td>Mrs Megala ArunKumar</td>')
     expect(rendered).to include('<td>25</td>')
     expect(rendered).to include('<td>Female</td>')
+    expect(rendered).to include('<td>2023-01-03</td>')
   end
 
   it 'includes delete links for each user' do
